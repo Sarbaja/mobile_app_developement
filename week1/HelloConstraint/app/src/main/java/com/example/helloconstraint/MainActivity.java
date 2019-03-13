6ddd508cdb09e7ebe39ff8eab84fe99ae2dbc7d7
Sarbaja <adhikari.sarbaja@gmail.com>
2019-03-13 10:29:29 +0545

    eleventh commit

--- week1/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ week1/HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloconstraint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,14 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int mcount =0;
     private TextView mShowCount;
-    private TextView zeroButton;
+    private Button zeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
-        zeroButton = (TextView) findViewById(R.id.zero);
+        zeroButton =  findViewById(R.id.zero);
     }
 
     public void showToast(View view) {
